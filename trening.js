bb29ade6761962f82bf5c4d4e55c22bea760f261
Alex <luzanaliaksandr@gmail.com>
2023-04-22 12:15:03 +0300

    mycom

--- trening.js
+++ trening.js
@@ -9,9 +9,7 @@ window.addEventListener('DOMContentLoaded', () => {
         second = stopwatch.querySelector('#second'),
         milisecond = stopwatch.querySelector('#milisecond'),
         zeroBlocks = document.querySelectorAll('.block');
-    
-
-    
+        
     function getZero(num) {
         if (num >= 0 && num < 10) {
             return `0${num}`;
@@ -20,42 +18,55 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
     
-    let timeInterval = false,
+    let countRedBut = 0,
         timerNew = 0,
-        countRedBut = 0;
-        
+        timeInterval;
+   
     butRed.addEventListener('click', () => {
         countRedBut += 1; 
-        if (countRedBut % 2 !== 0) {
+        butRed.classList.toggle('button-yellow');
+        if (countRedBut % 2 != 0) {
+            butRed.classList.remove('button-red');
             timeInterval = setInterval(function () {
-                const secondTime = Math.floor((timerNew / 100) % 60),
-                    minuteTime = Math.floor((timerNew / (100 * 60)) % 60),
-                    hourTime = Math.floor((timerNew * 60 * 60) % 24),
-                    milisecondTime = Math.floor(timerNew % 100);
-
-                timerNew += 1;
+            const secondTime = Math.floor((timerNew / 100) % 60),
+                minuteTime = Math.floor((timerNew / (100 * 60)) % 60),
+                hourTime = Math.floor((timerNew * 60 * 60) % 24),
+                milisecondTime = Math.floor(timerNew % 100);
+            timerNew += 1;
+                milisecond.innerHTML = getZero(milisecondTime);
                 second.innerHTML = getZero(secondTime);
                 minute.innerHTML = getZero(minuteTime);
-                hour.innerHTML = getZero(hourTime);
-                milisecond.innerHTML = getZero(milisecondTime);
-            }, 10);
-            butRed.classList.add('button-yellow');
-        }else {
+                hour.innerHTML = getZero(hourTime);           
+    }, 10);;
+        } else {
+            butRed.classList.add('button-red');
             clearInterval(timeInterval);
-            butRed.classList.toggle('button-yellow');
         }
+        
+        console.log(countRedBut);
     });      
-       
+  
     let countBlueBut = 0;
     butBlue.addEventListener('click', () => {
         countBlueBut += 1;
-        
-        zeroBlocks.forEach(elem => {
-            elem.innerHTML = '';
-            elem.innerHTML = '00';
-            
-        });
-        butRed.classList.toggle('button-yellow');
-    });
-  
+        if (countBlueBut >= 1) {
+            butRed.classList.remove('button-yellow');
+            butRed.classList.add('button-red');
+            countRedBut = 0;
+            clearInterval(timeInterval);
+            // zeroBlocks.forEach(elem => {
+            //     elem.innerHTML = '';
+            //     elem.innerHTML = '00';           
+            // });
+            timerNew = 0;
+            const secondTime = Math.floor((timerNew / 100) % 60),
+                minuteTime = Math.floor((timerNew / (100 * 60)) % 60),
+                hourTime = Math.floor((timerNew * 60 * 60) % 24),
+                milisecondTime = Math.floor(timerNew % 100);
+            milisecond.innerHTML = getZero(milisecondTime);
+            second.innerHTML = getZero(secondTime);
+            minute.innerHTML = getZero(minuteTime);
+            hour.innerHTML = getZero(hourTime);
+        }
+    }); 
 });
