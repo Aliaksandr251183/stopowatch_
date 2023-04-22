8919d97deef37c4755c3593cd2b2cee6f06030eb
Alex <1sharik144@gmail.com>
2023-04-22 09:38:15 +0300

    second

--- trening.js
+++ trening.js
@@ -19,51 +19,43 @@ window.addEventListener('DOMContentLoaded', () => {
             return num;
         }
     }
-    function redButton() {
-        let timeInterval = false,
-            timerNew = 0,
-            countRedBut = 0;
-            
-        butRed.addEventListener('click', () => {
-            countRedBut += 1; 
-            if (countRedBut % 2 !== 0 || countBlueBut >= 1) {
-                timeInterval = setInterval(function () {
-                    const secondTime = Math.floor((timerNew / 100) % 60),
-                        minuteTime = Math.floor((timerNew / (100 * 60)) % 60),
-                        hourTime = Math.floor((timerNew * 60 * 60) % 24),
-                        milisecondTime = Math.floor(timerNew % 100);
-
-                    timerNew += 1;
-                    second.innerHTML = getZero(secondTime);
-                    minute.innerHTML = getZero(minuteTime);
-                    hour.innerHTML = getZero(hourTime);
-                    milisecond.innerHTML = getZero(milisecondTime);
-                }, 10);
-                butRed.classList.add('button-yellow');
-                console.log(countBlueBut);
-            }else {
-                clearInterval(timeInterval);
-                butRed.classList.toggle('button-yellow');
-            }
-            console.log(countBlueRed);
-        });      
-    }
-
-    function blueButton() {
-        let countBlueBut = 0;
-        butBlue.addEventListener('click', () => {
-            countBlueBut += 1;
-           
-            zeroBlocks.forEach(elem => {
-                elem.innerHTML = '';
-                elem.innerHTML = '00';
-                
-            });
-            butRed.classList.toggle('button-yellow');
-        console.log(countBlueBut);
-        });
-    }
     
-    blueButton();
-    redButton();   
+    let timeInterval = false,
+        timerNew = 0,
+        countRedBut = 0;
+        
+    butRed.addEventListener('click', () => {
+        countRedBut += 1; 
+        if (countRedBut % 2 !== 0) {
+            timeInterval = setInterval(function () {
+                const secondTime = Math.floor((timerNew / 100) % 60),
+                    minuteTime = Math.floor((timerNew / (100 * 60)) % 60),
+                    hourTime = Math.floor((timerNew * 60 * 60) % 24),
+                    milisecondTime = Math.floor(timerNew % 100);
+
+                timerNew += 1;
+                second.innerHTML = getZero(secondTime);
+                minute.innerHTML = getZero(minuteTime);
+                hour.innerHTML = getZero(hourTime);
+                milisecond.innerHTML = getZero(milisecondTime);
+            }, 10);
+            butRed.classList.add('button-yellow');
+        }else {
+            clearInterval(timeInterval);
+            butRed.classList.toggle('button-yellow');
+        }
+    });      
+       
+    let countBlueBut = 0;
+    butBlue.addEventListener('click', () => {
+        countBlueBut += 1;
+        
+        zeroBlocks.forEach(elem => {
+            elem.innerHTML = '';
+            elem.innerHTML = '00';
+            
+        });
+        butRed.classList.toggle('button-yellow');
+    });
+  
 });
